1c1e38591ae76e7370674b9b300abb22f53553b7
maiconcesario <maiconwilli91@gmail.com>
2022-12-15 20:52:33 -0200

    Adicionando funcionalidade de mostrar a mensagem

--- cadastro.js
+++ cadastro.js
@@ -3,3 +3,9 @@ function sigUp(email){
 }
 
 //Pegando o email e cadstrando na plataforma
+
+function mostraMensagem(email){
+    return 'Bem vindo ao sistema';
+}
+
+//Adicionando funcao de mensagem
